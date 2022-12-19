d8c08979d3641362f9ef630a9e368f0f8158d5ef
TENG PHOUPHANIT <phouphanitteng@gmail.com>
2022-12-20 01:11:17 +0700

    done sidbar and navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,334 +17,17 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <style>
-        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap");
-
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: "Inter", sans-serif;
-        }
-
-        .sidebar {
-            position: fixed;
-            top: 0;
-            left: 0;
-            height: 100%;
-            width: 260px;
-            background: #ffff;
-            z-index: 100;
-            transition: all 0.5s ease;
-        }
-
-        .sidebar.close {
-            width: 78px;
-        }
-
-        .sidebar .logo-details {
-            height: 60px;
-            width: 100%;
-            display: flex;
-            align-items: center;
-        }
-
-        .sidebar .logo-details i {
-            font-size: 30px;
-            color: black;
-            height: 50px;
-            min-width: 78px;
-            text-align: center;
-            line-height: 50px;
-        }
-
-        .sidebar .logo-details .logo_name {
-            font-size: 22px;
-            color: black;
-            font-weight: 600;
-            transition: 0.3s ease;
-            transition-delay: 0.1s;
-        }
-
-        .sidebar.close .logo-details .logo_name {
-            transition-delay: 0s;
-            opacity: 0;
-            pointer-events: none;
-        }
-
-        .sidebar .nav-links {
-            height: 100%;
-            padding: 10px 0 150px 0;
-            overflow: auto;
-        }
-
-        .sidebar.close .nav-links {
-            overflow: visible;
-        }
-
-        .sidebar .nav-links::-webkit-scrollbar {
-            display: none;
-        }
-
-        .sidebar .nav-links li {
-            position: relative;
-            list-style: none;
-            transition: all 0.4s ease;
-        }
-
-        .sidebar .nav-links li:hover {
-            background: #5570F1;
-            color: #fff;
-            border-radius: 10px;
-        }
-
-        .sidebar .nav-links li .iocn-link {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-        }
-
-        .sidebar.close .nav-links li .iocn-link {
-            display: block;
-        }
-
-        .sidebar .nav-links li i {
-            height: 50px;
-            min-width: 78px;
-            text-align: center;
-            line-height: 50px;
-            color: black;
-            font-size: 20px;
-            cursor: pointer;
-            transition: all 0.3s ease;
-        }
-
-        .sidebar .nav-links li.showMenu i.arrow {
-            transform: rotate(-180deg);
-        }
-
-        .sidebar.close .nav-links i.arrow {
-            display: none;
-        }
-
-        .sidebar .nav-links li a {
-            display: flex;
-            align-items: center;
-            text-decoration: none;
-        }
-
-        .sidebar .nav-links li a .link_name {
-            font-size: 18px;
-            font-weight: 400;
-            color: black;
-            transition: all 0.4s ease;
-        }
-
-        .sidebar.close .nav-links li a .link_name {
-            opacity: 0;
-            pointer-events: none;
-        }
-
-        .sidebar .nav-links li .sub-menu {
-            padding: 6px 6px 14px 80px;
-            margin-top: -10px;
-            background: #1d1b31;
-            display: none;
-        }
-
-        .sidebar .nav-links li.showMenu .sub-menu {
-            display: block;
-        }
-
-        .sidebar .nav-links li .sub-menu a {
-            color: #11101d;
-            font-size: 15px;
-            padding: 5px 0;
-            white-space: nowrap;
-            opacity: 0.6;
-            transition: all 0.3s ease;
-        }
-
-        .sidebar .nav-links li .sub-menu a:hover {
-            opacity: 1;
-            color: #fff;
-        }
-
-        .sidebar.close .nav-links li .sub-menu {
-            position: absolute;
-            left: 100%;
-            top: -10px;
-            margin-top: 0;
-            padding: 10px 20px;
-            border-radius: 0 6px 6px 0;
-            opacity: 0;
-            display: block;
-            pointer-events: none;
-            transition: 0s;
-            background-color: #5570F1;
-        }
-
-        .sidebar.close .nav-links li:hover .sub-menu {
-            top: 0;
-            opacity: 1;
-            pointer-events: auto;
-            transition: all 0.4s ease;
-        }
-
-        .sidebar .nav-links li .sub-menu .link_name {
-            display: none;
-        }
-
-        .sidebar.close .nav-links li .sub-menu .link_name {
-            font-size: 18px;
-            opacity: 1;
-            display: block;
-        }
-
-        .sidebar .nav-links li .sub-menu.blank {
-            opacity: 1;
-            pointer-events: auto;
-            padding: 3px 20px 6px 16px;
-            opacity: 0;
-            pointer-events: none;
-        }
-
-        .sidebar .nav-links li:hover .sub-menu.blank {
-            top: 50%;
-            transform: translateY(-50%);
-            color: #e4e9f7;
-        }
-
-        .sidebar .profile-details {
-            position: fixed;
-            bottom: 0;
-            width: 260px;
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            background: white;
-            color: #11101d;
-            padding: 12px 0;
-            transition: all 0.5s ease;
-        }
-
-        .sidebar.close .profile-details {
-            background: none;
-        }
-
-        .sidebar.close .profile-details {
-            width: 78px;
-        }
-
-        .sidebar .profile-details .profile-content {
-            display: flex;
-            align-items: center;
-        }
-
-        .sidebar .profile-details img {
-            height: 52px;
-            width: 52px;
-            object-fit: cover;
-            border-radius: 16px;
-            margin: 0 14px 0 12px;
-            background: white;
-            transition: all 0.5s ease;
-        }
-
-        .sidebar.close .profile-details img {
-            padding: 10px;
-        }
-
-        .sidebar .profile-details .profile_name,
-        .sidebar .profile-details .job {
-            color: #111011;
-            font-size: 18px;
-            font-weight: 500;
-            white-space: nowrap;
-        }
-
-        .sidebar.close .profile-details i,
-        .sidebar.close .profile-details .profile_name,
-        .sidebar.close .profile-details .job {
-            display: none;
-        }
-
-        .sidebar .profile-details .job {
-            font-size: 12px;
-            color: #11101d;
-        }
-
-        .home-section {
-            position: relative;
-            background: #e4e9f7;
-            height: 100vh;
-            left: 260px;
-            width: calc(100% - 260px);
-            transition: all 0.5s ease;
-        }
-
-        .sidebar.close~.home-section {
-            left: 78px;
-            width: calc(100% - 78px);
-        }
-
-        .home-section .home-content {
-            height: 60px;
-            display: flex;
-            align-items: center;
-        }
-
-        .home-section .home-content .bx-menu,
-        .home-section .home-content .text {
-            color: #11101d;
-            font-size: 35px;
-        }
-
-        .home-section .home-content .bx-menu {
-            margin: 0 15px;
-            cursor: pointer;
-        }
-
-        .home-section .home-content .text {
-            font-size: 26px;
-            font-weight: 600;
-        }
-
-        @media (max-width: 400px) {
-            .sidebar.close .nav-links li .sub-menu {
-                display: none;
-            }
-
-            .sidebar {
-                width: 78px;
-            }
-
-            .sidebar.close {
-                width: 0;
-            }
-
-            .home-section {
-                left: 78px;
-                width: calc(100% - 78px);
-                z-index: 100;
-            }
-
-            .sidebar.close~.home-section {
-                width: 100%;
-                left: 0;
-            }
-        }
-    </style>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
 </head>
 
 <body>
     <div class="sidebar close">
-        <div class="logo-details mt-1 d-flex justify-content-center">
-            <img style="width: 2.2rem; height: 2.2rem;" src="https://itc.edu.kh/wp-content/uploads/2021/02/logoitc.png" alt="">
+        <div class="mt-2 pt-1 d-flex justify-content-center align-item-center">
+            <img style="width: 2.2rem; margin: none;" src="https://itc.edu.kh/wp-content/uploads/2021/02/logoitc.png" alt="">
         </div>
-        <ul class="nav-links">
+        <ul class="nav-links ">
             <li>
                 <a href="#">
                     <i class="bx bx-grid-alt"></i>
@@ -355,12 +38,19 @@
                 </ul>
             </li>
             <li>
-                <a href="#">
-                    <i class='bx bxs-folder-minus'></i>
-                    <span class="link_name">Inventory</span>
-                </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Inventory</a></li>
+                <div class="iocn-link">
+                    <a href="#">
+                        <i class='bx bxs-folder-minus'></i>
+                        <span class="link_name">Inventories</span>
+                    </a>
+                    <i class="bx bxs-chevron-down arrow"></i>
+                </div>
+                <ul class="sub-menu">
+                    <li><a class="link_name" href="#">Inventories</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Item</a></li>
+                    <li><a href="#">Transaction</a></li>
+                    <li><a href="#">Building</a></li>
                 </ul>
             </li>
             <li>
@@ -373,75 +63,68 @@
                 </ul>
             </li>
             <li>
-                <div class="profile-details">
-                    <div class="profile-content">
-                        <!-- <img src="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584__340.png" alt="profileImg"> -->
-                        <i class="bx bx-log-out"></i>
-                    </div>
-                    <div class="name-job">
-                        <div class="profile_name">Team 12</div>
-                        <div class="job">Web Desginer</div>
-                    </div>
-                </div>
+                <a class="mb-4 fixed-bottom" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                      document.getElementById('logout-form').submit();">
+                    <i style="font-size: 20px;" class="bx bx-log-out"></i>
+                    <span class="link_name">Logout</span>
+                </a>
             </li>
         </ul>
     </div>
     <section class="home-section">
         <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-                <div class="d-flex">
-                    <div class="home-content">
-                        <i class="bx bx-menu"></i>
-                        <span class="text">Inventory Management </span>
-                    </div>
-                    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="ml-auto collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav me-auto">
+            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm p-0">
+                <div class="home-content">
+                    <i class="bx bx-menu"></i>
+                    <span class="text">Inventory Management </span>
+                </div>
+                <div class="me-3 collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
 
-                        </ul>
+                    </ul>
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ms-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                            @if (Route::has('login'))
-                            <li class="nav-item">
-                                <!-- <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a> -->
-                            </li>
-                            @endif
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <!-- <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a> -->
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                            <li class="nav-item">
-                                <!-- <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a> -->
-                            </li>
-                            @endif
-                            @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <!-- <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a> -->
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('login') }}">
+                                    {{ __('Profile') }}
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                            @endguest
-                        </ul>
-                    </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
                 </div>
             </nav>
         </div>
-        <div class="bg-white m-3 rounded">
+        <div class="bg-white m-3 rounded p-3">
             @yield('content')
         </div>
     </section>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!-- Created by CodingLab |www.youtube.com/CodingLabYT-->
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+    <link rel="icon" href="https://itc.edu.kh/wp-content/uploads/2021/02/logoitc.png" />
+
+    <!-- Fonts -->
+    <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img style="width: 2.5rem;" src="https://itc.edu.kh/wp-content/uploads/2021/02/logoitc.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div class="navbar-nav ms-auto d-flex justify-content-center align-content-center">
+                        <span class="text-primary" style="font-size: 20px; font-weight: 600;">Institute Of Technology Of Cambodia</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+</body>
+
+</html>
