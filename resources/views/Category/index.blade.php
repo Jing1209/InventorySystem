0ecb54c5e2ab8be50309d274fc42397746364d89
voeundavii <voeundavii@gmail.com>
2023-01-05 10:28:39 +0700

    updating cate

--- resources/views/Category/index.blade.php
+++ resources/views/Category/index.blade.php
@@ -1,85 +1,86 @@
 @extends('layouts.app')
 @section('title')
-    Category
 @endsection
-
 @section('content')
-    <div class="bg-white p-3">
-        <div class="pull-right">
-            <a class="btn btn-success" href="{{ url('dashboard') }}"> Home</a>
-        </div>
-        <div class="container mt-2">
-            <div class="row">
-                <div class="col-lg-12 margin-tb">
-                    <div class="pull-left">
-                        <h2>Laravel 9 CRUD Example Tutorial</h2>
-                    </div>
-                    <div class="pull-right mb-2">
-                        <a class="btn btn-success" href="{{ route('categories.create') }}"> Create Category</a>
-                    </div>
-                </div>
+<div style="position: sticky;padding: 10px 0px 0 0px; top: 60px; overflow: hidden;background: #e4e9f7;" class="d-flex justify-content-between my-3">
+    <div class="w-25 d-flex justify-content-start text-white bg-primary rounded-2 me-2">
+        <a class="text-white " href="{{ url('dashboard') }}"> <i class='bx bx-home-alt p-2 m-3 rounded-2' style="background-color: rgba(255, 255, 255, 0.16); font-size: 18px;"></i></a>
+        <div class="mx-3 my-3">
+            All Category
+            <div>
+                234
             </div>
-            @if ($message = Session::get('success'))
-                <div class="alert alert-success">
-                    <p>{{ $message }}</p>
-                </div>
-            @endif
-
+        </div>
+    </div>
+    <div class=" w-75 d-flex align-items-center text-white bg-white rounded-2 me-2">
+        <div class="d-flex w-100 justify-content-between">
             {{-- search bar --}}
-            <div class="mx-auto pull-right">
-                <div class="">
-                    <form action="{{ route('categories.index') }}" method="GET" role="search">
-
-                        <div class="input-group">
-                            <span class="input-group-btn mr-5 mt-1">
-                                <button class="btn btn-info" type="submit" title="Search projects">
-                                    <span class="fas fa-search"></span>
-                                </button>
-                            </span>
-                            <input type="text" class="form-control mr-2" name="term" placeholder="Search projects"
-                                id="term">
-                            <a href="{{ route('categories.index') }}" class=" mt-1">
-                                <span class="input-group-btn">
-                                    <button class="btn btn-danger" type="button" title="Refresh page">
-                                        <span class="fas fa-sync-alt"></span>
-                                    </button>
-                                </span>
-                            </a>
-                        </div>
-                    </form>
+            <form class="ms-5 w-50" action="{{ route('categories.index') }}" method="GET" role="search">
+                <div class="d-flex justify-content-start">
+                    <div class="input-group">
+                        <input type="text" class="form-control mr-2 w-100 ps-3" name="term" placeholder="Search Category" id="term">
+                    </div>
+                    <span class="input-group-btn ms-2">
+                        <button class="btn btn-primary d-flex align-items-center h-100" type="submit" title="Search Category">
+                            <i style=" font-size: 18px;" class='bx bx-search'></i>
+                        </button>
+                    </span>
                 </div>
-            </div>
-
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>S.No</th>
-                        <th>Category Name</th>
-                        <th>Quantity</th>
-                        <th width="280px">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($categories as $category)
-                        <tr>
-                            <td>{{ $category->id }}</td>
-                            <td>{{ $category->category }}</td>
-                            <td>{{ $category->quantity }}</td>
-                            <td>
-                                <form action="{{ route('categories.destroy', $category->id) }}" method="Post">
-                                    <a class="btn btn-primary" href="{{ route('categories.edit', $category->id) }}">Edit</a>
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            </form>
+            <a class="me-5 text-white text-decoration-none" href="{{ route('categories.create') }}">
+                <div class="bg-primary cursor-pointer px-4 py-1 rounded-3 d-flex justify-conten-between">
+                    <div class="me-2 d-flex align-items-center">
+                        <i style="font-size: 18px;" class='bx bx-plus text-white'></i>
+                    </div>
+                    <span>Create Category</span>
+                </div>
+            </a>
         </div>
     </div>
-    <div class="d-flex justify-content-center">
-        {!! $categories->links() !!}
+</div>
+<div class="mt-1 rounded bg-white">
+    <div class="row">
     </div>
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+    @endif
+    <div class="m-3">
+        <table class="table table-borderless table-hover">
+            <thead class="border-bottom">
+                <tr>
+                    <!-- <th scope="col">S.No</th>
+                    <th scope="col">Category Name</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Action</th> -->
+                    <th class="col">S.No</th>
+                    <th class="col">Category Name</th>
+                    <th class="col">Quantity</th>
+                    <th class="col" style="width: 200px;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                <tr>
+                    <td>{{ $category->id }}</td>
+                    <td>{{ $category->category }}</td>
+                    <td>{{ $category->quantity }}</td>
+                    <td>
+                        <form action="{{ route('categories.destroy',$category->id) }}" method="Post">
+                            <a class="btn btn-primary" href="{{ route('categories.edit',$category->id) }}">Edit</a>
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="d-flex justify-content-center">
+    {!! $categories->links() !!}
+</div>
 @endsection
