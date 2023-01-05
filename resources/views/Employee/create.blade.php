cdee615ac5ba35ae122f234c97ce85720c079896
TENG PHOUPHANIT <phouphanitteng@gmail.com>
2023-01-05 22:24:32 +0700

    editing create employee

--- resources/views/Employee/create.blade.php
+++ resources/views/Employee/create.blade.php
@@ -4,75 +4,75 @@ Employee
 @endsection
 
 @section('content')
-<div class="pull-right mb-2">
-    <a class="btn btn-success" href="{{ url('home') }}"> Home</a>
-</div>
-<div class="container mt-2">
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left mb-2">
-                <h2>Add Company</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('employees.index') }}"> Back</a>
-            </div>
-        </div>
-    </div>
-    @if(session('status'))
-    <div class="alert alert-success mb-1 mt-1">
-        {{ session('status') }}
-    </div>
-    @endif
+<div style="height: 85vh;" class="m-3 mb-5 bg-white rounded-3">
     <form action="{{ route('employees.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Firstname:</strong>
-                    <input type="text" name="firstname" class="form-control" placeholder="Firstname">
-                    {{-- @error('name')
-                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}
+        <div class="d-flex justify-content-around">
+            <div class="row m-3">
+                <div class="d-flex justify-content-center">
+                    <div class="d-flex justify-content-start">
+                        <i style="font-size: 30px;" class='bx bxs-user'></i>
+                        <h3 class="ms-3">Create New Employee!</h3>
+                    </div>
                 </div>
-                @enderror --}}
+                <div class="">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <label for="lastname">Firstname</label>
+                            <input type="text" name="firstname" class="form-control" placeholder="Firstname">
+                            {{-- @error('name')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}
+                        </div>
+                        @enderror --}}
+                    </div>
+                </div>
+                <div class="">
+                    <div class="form-group">
+                        <label for="lastname">Lastname</label>
+                        <input type="text" name="lastname" class="form-control" placeholder="Lastname">
+                    </div>
+                </div>
+                <div class="">
+                    <label for="lastname">Gender</label>
+                    <select class="custom-select">
+                        <option value="Female">Female</option>
+                        <option value="Male">Male</option>
+                    </select>
+                </div>
+                <div class="">
+                    <div class="form-group">
+                        <label for="lastname">Email</label>
+                        <input type="email" name="email" class="form-control" placeholder="Email">
+                    </div>
+                </div>
+                <div class="">
+                    <div class="form-group">
+                        <label for="lastname">Phone number</label>
+                        <input type="number" name="phone_number" class="form-control" placeholder="Phone number">
+                    </div>
+                </div>
+                <div class="">
+                    <div class="">
+                        {{-- <label>Sponsor by:</label> --}}
+                    </div>
+                </div>
+                <div class="">
+                    <a class="btn btn-secondary" href="{{ route('employees.index') }}"> Back</a>
+                    <button type="submit" class="btn btn-primary ms-2"><i class="bx bx-save pe-1"></i>Save</button>
+                </div>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Lastname:</strong>
-                <input type="text" name="lastname" class="form-control" placeholder="Lastname">
-            </div>
+        <div class="">
+            <label>Choose Images</label>
+            <input type="file" name="images" onchange="loadFile(event)">
+            <img id="output" width="200" />
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Gender:</strong>
-                <select name="gender" id="">
-                    <option value="Female">Female</option>
-                    <option value="Male">Male</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email:</strong>
-                <input type="email" name="email" class="form-control" placeholder="Email">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Phone Number:</strong>
-                <input type="number" name="phone_number" class="form-control" placeholder="Phone number">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                {{-- <strong>Sponsor by:</strong> --}}
-                <label>Choose Images</label>
-                <input type="file"  name="images" >
-            </div>
-        </div>
-        
-        <button type="submit" class="btn btn-primary ml-3">Submit</button>
+    </form>
 </div>
-</form>
-</div>
+<script type="text/javascript">
+    var loadFile = function(event) {
+        var image = document.getElementById('output');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    };
+</script>
 @endsection
