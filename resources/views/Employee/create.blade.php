e26a9d28426132228de1ca7a650eeca80cad7b2c
TENG PHOUPHANIT <phouphanitteng@gmail.com>
2023-01-06 00:04:59 +0700

    form create employee done

--- resources/views/Employee/create.blade.php
+++ resources/views/Employee/create.blade.php
@@ -4,75 +4,78 @@ Employee
 @endsection
 
 @section('content')
-<div style="height: 85vh;" class="m-3 mb-5 bg-white rounded-3">
+<div style="height: 80vh;" class="m-3 mb-5 bg-white rounded-3">
     <form action="{{ route('employees.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="d-flex justify-content-around">
-            <div class="row m-3">
+            <div class="row m-5">
                 <div class="d-flex justify-content-center">
                     <div class="d-flex justify-content-start">
                         <i style="font-size: 30px;" class='bx bxs-user'></i>
                         <h3 class="ms-3">Create New Employee!</h3>
                     </div>
                 </div>
-                <div class="">
+                <div class="my-2">
                     <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
-                            <label for="lastname">Firstname</label>
-                            <input type="text" name="firstname" class="form-control" placeholder="Firstname">
+                            <label for="firstname">Firstname</label>
+                            <input type="text" name="firstname" class="form-control" placeholder="Input employee firstname">
                             {{-- @error('name')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}
                         </div>
                         @enderror --}}
                     </div>
                 </div>
-                <div class="">
+                <div class="my-2">
                     <div class="form-group">
                         <label for="lastname">Lastname</label>
-                        <input type="text" name="lastname" class="form-control" placeholder="Lastname">
+                        <input type="text" name="lastname" class="form-control" placeholder="Input employee lastname">
                     </div>
                 </div>
-                <div class="">
-                    <label for="lastname">Gender</label>
-                    <select class="custom-select">
+                <div class="mb-2">
+                    <label for="gender">Gender</label>
+                    <select  class="form-select" name="gender" id="">
                         <option value="Female">Female</option>
                         <option value="Male">Male</option>
                     </select>
+
                 </div>
-                <div class="">
+                <div class="mb-2">
                     <div class="form-group">
-                        <label for="lastname">Email</label>
-                        <input type="email" name="email" class="form-control" placeholder="Email">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control" placeholder="user@gmail.com">
                     </div>
                 </div>
-                <div class="">
+                <div class="mb-2">
                     <div class="form-group">
-                        <label for="lastname">Phone number</label>
-                        <input type="number" name="phone_number" class="form-control" placeholder="Phone number">
+                        <label for="phone-number">Phone number</label>
+                        <input type="number" name="phone_number" class="form-control" placeholder="087 xxxxxx32">
                     </div>
                 </div>
-                <div class="">
-                    <div class="">
-                        {{-- <label>Sponsor by:</label> --}}
-                    </div>
-                </div>
-                <div class="">
+                <div class="my-3">
                     <a class="btn btn-secondary" href="{{ route('employees.index') }}"> Back</a>
                     <button type="submit" class="btn btn-primary ms-2"><i class="bx bx-save pe-1"></i>Save</button>
                 </div>
             </div>
         </div>
-        <div class="">
-            <label>Choose Images</label>
-            <input type="file" name="images" onchange="loadFile(event)">
-            <img id="output" width="200" />
+        <div class="d-flex flex-column me-5">
+            <div style="margin-top: 100px;">
+                <img id="output" width="400" height="300" src="{{ asset('images/user.png') }}" />
+            </div>
+            <div class="d-flex my-3">
+                <input type="file" name="images" id="actual-btn" onchange="loadFile(event)" hidden>
+                <label style="cursor: pointer;" class="bg-success text-white p-2 rounded text-center" for="actual-btn">Choose File</label>
+                <span id="file-chosen" class="py-2 ps-2">No file chosen</span>
+            </div>
         </div>
     </form>
 </div>
 <script type="text/javascript">
+    var fileChosen = document.getElementById('file-chosen');
     var loadFile = function(event) {
         var image = document.getElementById('output');
         image.src = URL.createObjectURL(event.target.files[0]);
+        fileChosen.textContent = event.target.files[0].name
     };
 </script>
 @endsection
